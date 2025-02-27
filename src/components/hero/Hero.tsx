6b19f23a01d0a14f2e065977d8cbd37dc78d8af7
Boris Parrales <boris_parales@hotmail.com>
2025-02-27 17:23:00 -0500

    Cambio de imagen

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -32,7 +32,7 @@ export default function Hero () {
                     </div>
                     <Image
                         alt="Sponsor"
-                        src="/Sponsor1.png"
+                        src="/Sponsor.png"
                         width={50}
                         height={50}
                         className="mt-10 aspect-[6/5] w-full max-w-lg rounded-2xl object-fit sm:mt-16 lg:mt-0 lg:max-w-none xl:row-span-2 xl:row-end-2 xl:mt-36"
