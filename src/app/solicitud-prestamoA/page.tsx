d8e8f1dcbbe80bbeef35ae9d0416d0d874d825f2
System Administrator <boris_parales@hotmail.com>
2025-02-28 17:09:01 -0500

    Update changes

--- src/app/solicitud-prestamoA/page.tsx
+++ src/app/solicitud-prestamoA/page.tsx
@@ -1,5 +1,11 @@
+import { Metadata } from "next";
 import  {SolicitudPrestamoForm}  from "./ui/SolicitudPrestamoForm";
 
+
+export const metadata: Metadata = {
+  title: "CreditySalud",
+  description: "Nuestra misión es que puedas acceder a servicios médicos de calidad en procedimientos electivos y estéticos",
+};
 export default function SolicitudPage() {
   return (
     <SolicitudPrestamoForm/>
--- src/app/solicitud/page.tsx
+++ src/app/solicitud/page.tsx
@@ -1,5 +1,10 @@
+import { Metadata } from "next";
 import { SolicitudForm } from "./ui/SolicitudForm";
 
+export const metadata: Metadata = {
+  title: "CreditySalud",
+  description: "Nuestra misión es que puedas acceder a servicios médicos de calidad en procedimientos electivos y estéticos",
+};
 export default function SolicitudPage() {
   return (
     <SolicitudForm/>
