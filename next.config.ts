82f09ec01dac331133181ac6a774fab037f5b1d4
System Administrator <boris_parales@hotmail.com>
2025-02-28 20:38:31 -0500

    Update changes

--- next.config.ts
+++ next.config.ts
@@ -13,7 +13,7 @@ const nextConfig: NextConfig = {
   async headers() {
     return [
       {
-        source: "/api/email",
+        source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
